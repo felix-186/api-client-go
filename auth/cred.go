e7589ea1726f99876cf82739c70e0d71c258b63b
felix-186 <397435052@qq.com>
2026-07-10 11:21:14 +0800

    fix: api_key

--- auth/cred.go
+++ auth/cred.go
@@ -2,7 +2,6 @@ package auth
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/felix-186/api-client-go/apitransport"
 	"github.com/felix-186/api-client-go/config"
@@ -46,11 +45,11 @@ func (c *CustomCredential) HttpToken() middleware.Middleware {
 			transporter.RequestHeader().Set("Request-Type", "service")
 			token := transporter.RequestHeader().Get(config.XRequestHeaderAuthorization)
 			if token == "" {
-				token, err = (c.f)().Token()
+				token, err = (c.f)().Authorization()
 				if err != nil {
 					return nil, err
 				}
-				transporter.RequestHeader().Set(config.XRequestHeaderAuthorization, fmt.Sprintf("Bearer %s", token))
+				transporter.RequestHeader().Set(config.XRequestHeaderAuthorization, token)
 			}
 			return handler(ctx, req)
 		}
